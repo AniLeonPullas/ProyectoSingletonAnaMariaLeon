526adf5888b28db10accfc7c62fa038937bf13b9
Dennis Steven Ocaña Santamaria <dennis.ocana@udla.edu.ec>
2023-01-05 23:53:59 -0500

    Agregar archivos de proyecto.

---
+++ Singleton/ClaseSingletonAnaMariaLeon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class ClaseSingletonAnaMariaLeon
+    {
+        private static ClaseSingletonAnaMariaLeon instance = null;
+        public string mensaje="";
+
+        protected ClaseSingletonAnaMariaLeon()
+        {
+            mensaje = "Hola soy Anita";
+        }
+
+        public static ClaseSingletonAnaMariaLeon Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ClaseSingletonAnaMariaLeon();
+
+                return instance;
+            }
+        }
+
+    }
+}
---
+++ Singleton/ProgramaAnaMariaLeon.cs
@@ -0,0 +1,12 @@
+﻿namespace Singleton
+{
+    class ProgramaAnaMariaLeon
+    {
+        static void Main(string[] arga)
+        {
+            Console.WriteLine(ClaseSingletonAnaMariaLeon.Instance.mensaje);
+            ClaseSingletonAnaMariaLeon.Instance.mensaje = "Hola soy Maria";
+            Console.WriteLine(ClaseSingletonAnaMariaLeon.Instance.mensaje);
+        }
+    }
+}
